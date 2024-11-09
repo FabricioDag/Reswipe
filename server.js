f57ec7a32aebc2d40d28b14259405e1cdabf7192
Fabrício DAgostini <devfabriciodag@gmail.com>
2024-11-09 18:09:40 -0300

    dotEnv

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = require('./app') // importa o arquivo app.js
 const mongoose = require('mongoose')
 
 //Credentials
-const dbUser = 'devfabriciodag'
-const dbPassword = 'kBVkl7KpQK1gxAJo' // mover para o .env
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS // mover para o .env
 
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.pqilv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     console.log('Conectou ao banco')
-    const PORT = 3000;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
 .catch(error => console.error('Database connection error:', error));
